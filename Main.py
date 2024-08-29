f6b7dfc35c18990187e45c512c72222a883b2ac8
Ondřej Seidl <seidon@seznam.cz>
2024-08-29 15:16:23 +0200

    Adding next pages

--- Main.py
+++ Main.py
@@ -15,5 +15,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(145, 0, 180)
     pdf.cell(w=0, h=15, ln=1,  txt=row["Topic"])
     pdf.line(10,22, 200, 22)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 pdf.output('output.pdf')
 
