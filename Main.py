d4b0b5bf66003bbd451134d886f4e508464c04a6
Ondřej Seidl <seidon@seznam.cz>
2024-08-29 10:46:52 +0200

    Creating headers + style

--- Main.py
+++ Main.py
@@ -1,14 +1,19 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
+df = pd.read_csv('topics.csv')
+
 pdf.add_page()
-
-pdf.set_font('Arial', 'B', size=20)
+pdf.set_font('Arial', 'B', size=24)
 pdf.cell(w=0, h=0, ln=1, align='C', txt='PDF Generator')
-pdf.cell(w=0, h=15, ln=1)
 
-pdf.set_font('Arial', 'B', size=16)
-pdf.cell(w=0, h=16, ln=1, align='L', txt='Example text', border=1)
-
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', size=20)
+    pdf.set_text_color(145, 0, 180)
+    pdf.cell(w=0, h=15, ln=1,  txt=row["Topic"])
+    pdf.line(10,22, 200, 22)
 pdf.output('output.pdf')
+
